c0a41b4291d0eab8baa805b46e2fbf6d520aaf55
faprikaa <muammarm28@gmail.com>
2023-12-06 23:22:30 +0700

    Test

--- MobileDevelopment/app/src/main/java/com/cropoptima/cropoptima/main/MainActivity.kt
+++ MobileDevelopment/app/src/main/java/com/cropoptima/cropoptima/main/MainActivity.kt
@@ -35,6 +35,6 @@ MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
-
+    // test commit and push
 
 }
